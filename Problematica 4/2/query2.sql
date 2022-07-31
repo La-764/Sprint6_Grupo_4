3f3dec2469589dbaae8b84a5d39e27d781f66c3f
Rocio <rochimarivega@hotmail.com>
2022-07-31 17:09:12 -0300

    .

--- Problematica 4/2/query2.sql
+++ Problematica 4/2/query2.sql
@@ -1,6 +1,15 @@
 --Obtener la cantidad de empleados por cliente por sucursal en un número real
-SELECT CAST(count(e.employee_id) AS FLOAT) as cantidad_empleados, c.customer_id as cliente, s.branch_name as nombre_sucursal
-FROM cliente as c
-JOIN sucursal as s ON s.branch_id=c.branch_id 
-JOIN empleado as e ON e.branch_id=s.branch_id 
-group by c.customer_id, s.branch_name;
+create view clientes_sucursal as 
+select  branch_id, count(customer_id) as total_clientes
+from cliente
+group by branch_id;
+create view empleados_sucursal as 
+select branch_id, count(employee_id) as total_empleados
+from empleado
+group by branch_id;
+
+select s.branch_name as nombre_sucursal,  CAST(total_clientes as REAL) / total_empleados as empleados_por_cliente
+FROM clientes_sucursal cs
+join empleados_sucursal es on cs.branch_id = es.branch_id
+JOIN sucursal as s ON s.branch_id=cs.branch_id 
+group by s.branch_name;
